8895360327777d6c45d3ed02c564a1102021afde
munenepeter <peternjega7@gmail.com>
2023-12-30 19:55:46 +0300

    refactor openning dir to a fn

--- ls.c
+++ ls.c
@@ -5,31 +5,35 @@
 #include <string.h>
 #include <errno.h>
 
-int main(int argc, char** argv) {
-
-    //Open dir
-    const char* dir_path = ".";
-    DIR* dir_path_p = opendir(dir_path);
+void print_files_in_dir(const char* directory_path) {
+    DIR* dir_path_p = opendir(directory_path);
 
     if (dir_path_p == NULL) {
-        fprintf(stderr, "E: Could not open %s:%s", dir_path, strerror(errno));
+        fprintf(stderr, "E: Could not open %s:%s", directory_path, strerror(errno));
         exit(-1);
     }
+    //reset the errno
     errno = 0;
 
     //read dir
     struct dirent* dir_entry = readdir(dir_path_p);
 
     if (errno != 0) {
-        fprintf(stderr, "E: Could not read %s:%s", dir_path, strerror(errno));
+        fprintf(stderr, "E: Could not read %s:%s", directory_path, strerror(errno));
         exit(-1);
     }
     //list items
-
     while (dir_entry != NULL) {
         printf("file: %s\n", dir_entry->d_name);
         dir_entry = readdir(dir_path_p);
     }
+}
+
+int main(int argc, char** argv) {
+
+    //Open dir
+    const char* dir_path = ".";
+    print_files_in_dir(dir_path);
 
     return 0;
 }
