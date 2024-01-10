c58e20b3cc2f4bed7d195359f53b6877fa1721d1
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2024-01-10 20:31:13 +0300

    chore: add pseudo code for basic flow

--- src/cat.c
+++ src/cat.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*
+function cat(files):
+    for each file in files:
+        open file
+        while not end of file:
+            read line from file
+            print line
+        close file
 
-
-
+*/
 int main(int argc, char** argv) {
     (void)argc;
     (void)argv;
